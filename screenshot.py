dbb1f51a7ca6bd9bb343cd1c74d70cbc072085bd
jbernadas <jb3rns@gmail.com>
2019-06-14 10:08:56 -0500

    Added new feature wherein the script will ask you if you want to make changes to the generated sitemap.txt before it proceeds to take screenshots

--- screenshot.py
+++ screenshot.py
@@ -34,27 +34,32 @@ exclude3 = 'pdf'
 # # please refer to http://toncar.cz/opensource/sitemap_gen.html for additional info on how to use sitemap_gen.py
 os.system('python3 ./sitemap_gen/sitemap_gen.py -b %s -b %s -b %s %s' %(exclude1, exclude2, exclude3, target_url))
 
-class Test(unittest.TestCase):
-    """ Demonstration: Get Chrome to generate fullscreen screenshot """
+edit_sitemap = input('Would you like to edit the newly generated sitemap before I proceed to take screenshots? ')
+if edit_sitemap == "yes":
+    # Do this.
+    print("Ok, I'll wait.")
+    itsago = input("Type go when you are ready? ")
+    if itsago == "go":
+        class Test(unittest.TestCase):
+            """ Demonstration: Get Chrome to generate fullscreen screenshot """
 
-    def setUp(self):
-        self.driver = webdriver.Chrome()
+            def setUp(self):
+                self.driver = webdriver.Chrome()
 
-    def tearDown(self):
-        self.driver.quit()
+            def tearDown(self):
+                self.driver.quit()
 
-    def test_fullpage_screenshot(self):
-        ''' Generate document-height screenshot '''
-        
-        i = 1
-        with open("sitemap.txt") as urls:
-            for line in urls:
-                self.driver.get(line)
-                self.driver.set_window_size(1440, 2000)
-                util.fullpage_screenshot(self.driver, "./website-screenshots/page" + str(i) + ".png")  
-                i = i + 1
-        urls.close()
+            def test_fullpage_screenshot(self):
+                ''' Generate document-height screenshot '''
+                
+                i = 1
+                with open("sitemap.txt") as urls:
+                    for line in urls:
+                        self.driver.get(line)
+                        self.driver.set_window_size(1440, 2000)
+                        util.fullpage_screenshot(self.driver, "./website-screenshots/page" + str(i) + ".png")  
+                        i = i + 1
+                urls.close()
 
 if __name__ == "__main__":
     unittest.main(argv=[sys.argv[0]])
-    
